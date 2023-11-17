94e4201e31125487c3df275dcfd91797252db7f7
Gary Gudini <garygudini@protonmail.com>
2023-11-17 18:20:23 +0300

    added test case

---
+++ tests/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Mock;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testMock(): void
+    {
+        $this->assertTrue(true);
+    }
+}
